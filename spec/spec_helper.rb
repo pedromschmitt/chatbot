27c4ff5cec3b4aaa728b5e6087ab6db092091481
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-04-01 16:59:25 -0300

    Update spec to use factories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 # spec/spec_helper.rb
 require 'rack/test'
 require 'rspec'
+require 'ffaker'
+require 'pg_search'
 require_relative '../app.rb'
+
+Dir["./spec/support/**/*.rb"].each { |f| require f }
+Dir["./app/services/**/*.rb"].each {|file| require file }
 
 set :environment, :test
 
@@ -16,4 +21,6 @@ end
 #RSpec.configure { |c| c.include RSpecMixin }
 RSpec.configure do |c|
   c.include RSpecMixin
+  ActiveRecord::Base.logger = nil
+  ENV['LOG'] == true
 end
